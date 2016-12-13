1d8f782c4e447083aff2dcfb24654cb592b0bf5d
DESKTOP-43EH4JU\quoc_trinh <quoc_trinh@lampart-vn.com>
2016-12-13 08:52:47 +0700

    first commit

---
+++ application/controllers/Hello.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: quoc_trinh
+ * Date: 12/12/2016
+ * Time: 15:25
+ */
+class Hello
+{
+
+}
---
+++ application/controllers/Home.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: quoc_trinh
+ * Date: 13/12/2016
+ * Time: 08:31
+ */
+class Home
+{
+
+}
